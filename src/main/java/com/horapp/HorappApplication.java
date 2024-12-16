e04b86d5559eb6ae2182eaeb690defc8d05c9095
Cabaleiro Nicolas <nicocabaleiro2002@gmail.com>
2024-12-16 10:58:07 -0300

    first commit

---
+++ src/main/java/com/horapp/HorappApplication.java
@@ -0,0 +1,13 @@
+package com.horapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorappApplication.class, args);
+	}
+
+}
